22d74df9c8ae6189338aa36e182c72093847149f
shinyay <shinya.com@gmail.com>
2020-06-15 12:54:56 +0900

    Add Access App on GKE through Ingress

---
+++ spring/script/gke-ingress-access.fish
@@ -0,0 +1,18 @@
+#!/usr/bin/env fish
+
+function do_func
+  argparse -n do_func 'h/help' 't/target=' -- $argv
+  or return 1
+
+  if set -lq _flag_help
+    echo "gke-loadbalancer-access.fish -t/--target <TARGET_ENDPOINT_IP>"
+    return
+  end
+
+  set -lq _flag_target
+  or set -l _flag_target (kubectl get ingress hello-gke-ingress -o json | jq .status.loadBalancer.ingress[0].ip -r)
+
+  curl $_flag_target
+end
+
+do_func $argv
