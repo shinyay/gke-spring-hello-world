7a357a1c53b4aa481a85640691f2fa517fdb1e88
shinyay <shinya.com@gmail.com>
2020-06-15 10:27:32 +0900

    Add Deployment YAML configuration

---
+++ spring/script/gke-deployment-config.fish
@@ -0,0 +1,20 @@
+#!/usr/bin/env fish
+
+function do_func
+  argparse -n do_func 'h/help' 'p/project=' -- $argv
+  or return 1
+
+  if set -lq _flag_help
+    echo "gke-deployment-config.fish -p/--project <DOCKERFILE_DIRECTORY>"
+    return
+  end
+
+  set -lq _flag_project
+  or set -l _flag_project (gcloud config get-value project)
+  
+  set -x GOOGLE_CLOUD_PROJECT $_flag_project
+  envsubst < (pwd |awk -F '/gke-spring-hello-world' '{print $1}')/gke-spring-hello-world/spring/script/deployment.yml.template \
+    > (pwd |awk -F '/gke-spring-hello-world' '{print $1}')/gke-spring-hello-world/spring/script/deployment.yml
+end
+
+do_func $argv
