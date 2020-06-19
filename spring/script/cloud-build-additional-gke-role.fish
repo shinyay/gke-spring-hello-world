c75980ee3b975287eaa6a42d62141fb30f99d26e
shinyay <shinya.com@gmail.com>
2020-06-19 21:22:20 +0900

    Add Role description for GKE

--- spring/script/cloud-build-additional-gke-role.fish
+++ spring/script/cloud-build-additional-gke-role.fish
@@ -12,12 +12,14 @@ function do_func
   set -lq _flag_account
   or set -l _flag_account (gcloud projects get-iam-policy (gcloud config get-value project)| grep cloudbuild.gserviceaccount.com | uniq | cut -d ':' -f 2)
 
+## Role for GKE
 # roles/container.admin - Provides access to full management of clusters and their Kubernetes API objects
 # roles/container.clusterAdmin - Provides access to management of clusters
 # roles/container.clusterViewer - Read-only access to Kubernetes Clusters
 # roles/container.developer - Provides access to Kubernetes API objects inside clusters
 # roles/container.hostServiceAgentUser - Allows the Kubernetes Engine service account in the host project to configure shared network resources for cluster management
 # roles/container.viewer - Provides read-only access to GKE resources
+
   gcloud projects add-iam-policy-binding (gcloud config get-value project) --member serviceAccount:$_flag_account --role roles/container.admin
   gcloud projects add-iam-policy-binding (gcloud config get-value project) --member serviceAccount:$_flag_account --role roles/source.writer
 
